ddd3d1cb20748e0a457b58425403870826f6cefd
sergio vieira <sergiosvieira@gmail.com>
2021-06-23 20:08:30 -0300

    [+] Data types and converting types

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // ***** VARIABLES AND MUTABILITY *****
     //1. by default variables are immutable
     // variable can be infered by value or by type
     // Valid types: Integer: signed (i8, i16, i32, i64, i128 and isize [architecture dependent])
@@ -23,8 +24,9 @@ fn main() {
     let mut _m: i32;
     // println!("m:{}", _m); <--10. error: use of possibly-uninitialized `_m`
     _m = 10;
-    println!("m:{}", _m);
-    // 10. constants are always immutables
+    println!("_m:{}", _m);
+    // ***** CONSTANTS *****
+    // 10. constants are aways immutables
     // const mut n = 10; // <--11. error: expected one of `!`, `.`, `::`, `;`, `?`, `{`, `}`,
     //                             or an operator. Cannot be mutable
     // const n = 10; //<--12. error: When you use const keyword you must provide a type for the 
@@ -34,6 +36,45 @@ fn main() {
     const N:i32 = 5;
     println!("N:{}", N);
     //const M:i32 = _m; // error: attempt to use a non-constant value in a constant
-    let _n = N;// valid: you can use a constant value in a immutable variable
-    println!("_n:{}", _n);
+    let _n = N;
+    println!("_n:{}", _n);// valid: you can use a constant value in a immutable variable
+    // ***** SHADOWING *****
+    // you can use the same name for different declarations of immutable variables
+    let _n = _n + _m; // _n remains immutable after this transformation
+    println!("_n + _m:{}", _n);
+    // better usage sample
+    let text = "this is a string";
+    let text = text.len();
+    // you don't need to create two variables to store the string (text_str) and len (text_len) 
+    // of the string
+    println!("text length:{}", text);
+    // let mut text = "this is a string";
+    // let mut text = text.len(); // error: expected `&str`, found `usize`
+    // ***** DATA TYPES *****
+    // Rust is statically typed language, so it must know the type of all variables at compile time.
+    // If you want convert a string to other type you can use the method parse
+    // but you must to specify the type of variable
+    // let guess = "42".parse(); // error: cannot infer type for type parameter `F` declared on the 
+    //                              associated function `parse`    
+    //let guess: i32 = "42".parse();// error: expected `i32`, found enum `Result`
+    // The parse method return an enum `Result`.
+    // Result is a type that represents either success (Ok) or failure (Err)
+    // So, to get the result of conversion you must use something like this
+    let r: Result<i32, std::num::ParseIntError> = "42".parse();
+    println!("{:?}", r);
+    // From the result of conversion (Result<i32, std::num::ParseIntError>) you can get the converted value
+    if r.is_ok() {
+        println!("converted:{}", r.unwrap());
+    } else {
+        println!("Conversion error");
+    }
+    // Finally, you can do the conversion directly
+    let r: i32 = "42".parse().unwrap();
+    println!("r:{}", r);
+    // you can use the turbofish sintax
+    let r = "42".parse::<i32>().unwrap();
+    println!("r:{}", r);
+    // if you get an invalid integer, that will generate a conversion error
+    let r = "value".parse::<i32>().unwrap();// error: ParseIntError { kind: InvalidDigit }'
+    println!("r:{}", r);
 }
